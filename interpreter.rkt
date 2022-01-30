a402c2e348e1312c149fbbebd18dc48da5b428a3
amirmohammad ghasemi <ghasemiamirmohammad@yahoo.com>
2022-01-30 15:54:55 +0330

    Add more datatypes

--- interpreter.rkt
+++ interpreter.rkt
@@ -25,36 +25,44 @@
 ; datatypes
 
 (define-datatype Program Program?
-    (a-program
+    (program
         (statements (non-empty-list-of Statement?))))
 
 
 (define-datatype Statement Statement?
-    (a-compound-statement
+    (compound-statement-statement
         (compound-statement Compound-Statement?))
-    (a-simple-statement
+    (simple-statement-statement
         (simple-statement Simple-Statement?))
     )
 
-
-(define-datetype Simple-Statement Simple-Statement?
-    (an-assignment
+(define-datatype Simple-Statement Simple-Statement?
+    (assignment-simple-statement
         (assignment Assignment?))
-    (a-return-statement
-        (return-statement Return-Statement))
-    (a-pass-statement)
-        ()
-    )
+    (return-simple-statement
+        (return-statement Return-Statement?))
+    (pass-simple-statement))
 
-(define-datetype Compound-Statement Compound-Statement?
-    (a-function-definition
-        (function-definition Function-Definition?))
-    (a-if-statement
+(define-datatype Compound-Statement Compound-Statement?
+;    (a-function-definition
+;        (function-definition Function-Definition?))
+    (an-if-statement
         (if-statement If-Statement?))
 ;    (a-for-statement
 ;        (for-statement For-Statement?))
     )
 
+
+(define-datatype Assignment Assignment?
+    (assignment
+        (id symbol?)
+        (expression Expression?))
+    )
+
+(define-datatype Return-Statement Return-Statement?
+	(simple-return)
+	(expression-return
+		(expression Expression?)))
 #|
 
 (define-datetype Function-Definition Function-Definition?
@@ -67,3 +75,88 @@
 
 
 |#
+
+
+
+(define-datatype If-Statement If-Statement?
+	(if-statement
+		(if-expression Expression?)
+		(if-body (non-empty-list-of Statement?))
+		(else-body Else-Block?)))
+
+(define-datatype Else-Block Else-Block?
+	(else-block
+		(else-statements (non-empty-list-of Statement?))))
+
+
+(define-datatype Expression Expression?
+	(disjunction-expression
+		(disjunction Disjunction?))
+	(sum-expression
+		(sum Sum?)))
+
+(define-datatype Disjunction Disjunction?
+	(conjunction-expression
+		(conjunction Conjunction?))
+	(or-expression
+		(disjunction Disjunction?)
+		(conjunction Conjunction?)))
+	
+
+(define-datatype Conjunction Conjunction?
+	(inversion-expression
+		(inversion Inversion?))
+	(and-expression
+		(conjunction Conjunction?)
+		(inversion Inversion?)))
+
+(define-datatype Inversion Inversion?
+	(simple-inversion
+		(inversion Inversion?))
+	(comparison-expression
+		(comparison Comparison?)))
+
+
+(define-datatype Comparison Comparison?
+	(eq-sum
+		(left-sum Sum?)
+		(right-sum Sum?))
+	(lt-sum
+		(left-sum Sum?)
+		(right-sum Sum?))
+	(gt-sum
+		(left-sum Sum?)
+		(right-sum Sum?)))
+	
+(define-datatype Sum Sum?
+	(addition
+		(sum Sum?)
+		(term Sum?))
+	(subtraction
+		(sum Sum?)
+		(term Term?))
+	(term-sum
+		(term Term?)))
+
+
+(define-datatype Term Term?
+	(muliplication
+		(term Term?)
+		(factor Factor?))
+	(division
+		(term Term?)
+		(factor Factor?))
+	(factor-term
+		(factor Factor?)))
+
+
+(define-datatype Factor Factor?
+	(plus
+		(factor Factor?))
+	(minus
+		(factor Factor?))
+;	(power-factor
+;		(power Power?))
+		)
+		
+
